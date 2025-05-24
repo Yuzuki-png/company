9b26a3a2e89b4ec718fe1b92cf61a334c8690312
Yuzuki Nishi <youyuexi196@gmail.com>
2025-05-24 17:34:19 +0900

    fix: 利用規約、プライバシーポリシー修正

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function PrivacyPolicy() {
   return (
-    <div className="pt-24 pb-20">
+    <div className="pt-24 pb-20 bg-black min-h-screen">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl md:text-5xl font-bold mb-12 text-center text-white">
           プライバシーポリシー
--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function TermsOfService() {
   return (
-    <div className="pt-24 pb-20">
+    <div className="pt-24 pb-20 bg-black">
       <div className="container mx-auto px-4">
         <h1 className="text-4xl md:text-5xl font-bold mb-12 text-center text-white">
           利用規約
